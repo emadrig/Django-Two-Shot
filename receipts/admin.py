fb6b5e8772b3ec1b0ec1c79fb166811efe347c93
Edwin <emadrig1@uci.edu>
2022-12-05 14:36:35 -0800

    Feature 13 Complete

--- receipts/admin.py
+++ receipts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from receipts.models import ExpenseCategory, Receipt, Account
 
+
 # Register your models here.
 @admin.register(ExpenseCategory)
 class ExpenseCategoryAdmin(admin.ModelAdmin):
--- receipts/urls.py
+++ receipts/urls.py
@@ -4,11 +4,15 @@ from receipts.views import (
     account_list,
     category_list,
     create_receipt,
+    create_category,
+    create_account,
 )
 
 urlpatterns = [
     path("", receipt_list, name="home"),
-    path("create/", create_receipt, name="create_receipt"),
     path("categories/", category_list, name="category_list"),
     path("accounts/", account_list, name="account_list"),
+    path("create/", create_receipt, name="create_receipt"),
+    path("categories/create/", create_category, name="create_category"),
+    path("accounts/create/", create_account, name="create_account"),
 ]
