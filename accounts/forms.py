183b921530ace50cccb6a09f323b32e6a240b07a
Edwin <emadrig1@uci.edu>
2022-12-05 12:41:03 -0800

    Feature 10 Complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,16 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150)
+    password = forms.CharField(max_length=150, widget=forms.PasswordInput)
+
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(max_length=150)
     password = forms.CharField(
         max_length=150,
-        widget=forms.PasswordInput
-        )
+        widget=forms.PasswordInput,
+    )
+    password_confirmation = forms.CharField(
+        max_length=150,
+        widget=forms.PasswordInput,
+    )
--- receipts/models.py
+++ receipts/models.py
@@ -18,6 +18,9 @@ class ExpenseCategory(models.Model):
         on_delete=models.CASCADE,
     )
 
+    def __str__(self):
+        return self.name
+
 
 class Account(models.Model):
     name = models.CharField(max_length=100)
@@ -28,6 +31,9 @@ class Account(models.Model):
         related_name="accounts",
         on_delete=models.CASCADE,
     )
+
+    def __str__(self):
+        return self.name
 
 
 class Receipt(models.Model):
@@ -50,3 +56,6 @@ class Receipt(models.Model):
     account = models.ForeignKey(
         Account, related_name="receipts", on_delete=models.CASCADE, null=True
     )
+
+    def __str__(self):
+        return self.vendor
