5f7d3db14c1397eb5d9069d564b05989b086014b
Edwin <emadrig1@uci.edu>
2022-12-05 13:57:27 -0800

    Feature 12 Complete

--- receipts/views.py
+++ receipts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Receipt
+from .models import Receipt, ExpenseCategory, Account
 from .forms import ReceiptForm
 from django.contrib.auth.decorators import login_required
 
@@ -12,6 +12,22 @@ def receipt_list(request):
     context = {"receipt_list": receipt_list}
 
     return render(request, "receipts/list.html", context)
+
+
+@login_required
+def category_list(request):
+    category_list = ExpenseCategory.objects.filter(owner=request.user)
+    context = {"category_list": category_list}
+
+    return render(request, "categories/list.html", context)
+
+
+@login_required
+def account_list(request):
+    account_list = Account.objects.filter(owner=request.user)
+    context = {"account_list": account_list}
+
+    return render(request, "accounts/list.html", context)
 
 
 @login_required
